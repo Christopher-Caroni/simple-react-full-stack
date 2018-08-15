729e3db5991864bf66cd38f2b47e4b1470ea8145
Christopher Caroni <christopher.caroni@etudiant.univ-lille1.fr>
2018-08-15 18:05:06 +0200

    created authentication flow for redux

---
+++ src/client/actions/Auth/authActions.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import authConstants from '../../constants/Auth/authConstants';
+
+const USER_API_URL = '/api/auth';
+
+function loginRequest(user) {
+    return {
+        type: authConstants.LOGIN_REQUEST,
+        payload: { user },
+    };
+}
+
+function loginSuccess(user) {
+    return {
+        type: authConstants.LOGIN_SUCCESS,
+        payload: { user },
+    };
+}
+
+function loginFailure(error) {
+    return {
+        type: authConstants.LOGIN_FAILURE,
+        payload: { error },
+    };
+}
+
+export function login(user) {
+    return dispatch => {
+        dispatch(loginRequest(user));
+
+        return axios.post(`${USER_API_URL}/login`, user).then(
+            res => {
+                dispatch(loginSuccess(res.data));
+            },
+            err => {
+                dispatch(loginFailure(err));
+            }
+        );
+    };
+}
+
+export function refreshCurrentUser() {
+    return dispatch =>
+        axios.get(`${USER_API_URL}`).then(
+            res => {
+                dispatch(loginSuccess(res.data));
+            },
+            err => {
+                dispatch(loginFailure(err));
+            }
+        );
+}
---
+++ src/client/reducers/Auth/authReducer.js
@@ -0,0 +1,52 @@
+import authConstants from '../../constants/Auth/authConstants';
+
+const initialUser = {
+    username: '',
+};
+
+const initialState = {
+    authenticated: false,
+    loggingIn: false,
+    user: {
+        username: '',
+    },
+};
+
+export default function userReducer(state = initialState, action) {
+    const {
+        payload: { error: loginError, user } = {
+            error: new Error('Encountered unknown error while logging in'),
+            user: initialUser,
+        },
+    } = action;
+
+    switch (action.type) {
+        case authConstants.LOGIN_REQUEST:
+            return {
+                ...state,
+                auth: {
+                    loggingIn: true,
+                },
+            };
+        case authConstants.LOGIN_FAILURE:
+            return {
+                ...state,
+                auth: {
+                    loggingIn: false,
+                    authenticated: false,
+                    loginError,
+                },
+            };
+        case authConstants.LOGIN_SUCCESS:
+            return {
+                ...state,
+                auth: {
+                    loggingIn: false,
+                    authenticated: true,
+                    user,
+                },
+            };
+        default:
+            return state;
+    }
+}
