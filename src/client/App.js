e176b07dca6dbd36c802612f181d36afdf9bb36c
Christopher Caroni <christopher.caroni@etudiant.univ-lille1.fr>
2018-08-15 11:47:54 +0200

    added prettier config

--- src/client/App.js
+++ src/client/App.js
@@ -1,29 +1,29 @@
-import React, { Component } from "react";
-import "./app.css";
-import ReactImage from "./react.png";
+import React, { Component } from 'react';
+import './app.css';
+import ReactImage from './react.png';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { username: null };
-  }
+    constructor(props) {
+        super(props);
+        this.state = { username: null };
+    }
 
-  componentDidMount() {
-    fetch("/api/getUsername")
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
+    componentDidMount() {
+        fetch('/api/getUsername')
+            .then(res => res.json())
+            .then(user => this.setState({ username: user.username }));
+    }
 
-  render() {
-    return (
-      <div>
-        {this.state.username ? (
-          <h1>Hello {this.state.username}</h1>
-        ) : (
-          <h1>Loading.. please wait!</h1>
-        )}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                {this.state.username ? (
+                    <h1>Hello {this.state.username}</h1>
+                ) : (
+                    <h1>Loading.. please wait!</h1>
+                )}
+                <img src={ReactImage} alt="react" />
+            </div>
+        );
+    }
 }
