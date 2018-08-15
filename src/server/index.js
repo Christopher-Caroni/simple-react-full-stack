adcb5e0e73aff3176b7c57b835e948c69b448eec
Christopher Caroni <christopher.caroni@etudiant.univ-lille1.fr>
2018-08-15 13:56:24 +0200

    created authentication routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,25 @@
+import './db';
+import './middlewares';
 import winston from './config/winston';
+import AuthRouter from './routes/auth';
 
 const express = require('express');
 const os = require('os');
 const morgan = require('morgan');
+const passport = require('passport');
 
 const app = express();
 
 app.use(morgan('combined', { stream: winston.stream }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api/auth', AuthRouter);
+
 app.use(express.static('dist'));
+
 app.get('/api/getUsername', (req, res) =>
     res.send({ username: os.userInfo().username })
 );
+
 app.listen(8080, () => console.log('Listening on port 8080!'));
---
+++ src/server/routes/auth/index.js
@@ -0,0 +1,66 @@
+import User from '../../db/models/User';
+
+import LOCAL_STRATEGY_KEY from '../../middlewares/passport';
+
+const express = require('express');
+const passport = require('passport');
+
+const AuthRouter = express.Router();
+
+export const loggedInOnly = (req, res, next) => {
+    if (req.isAuthenticated()) next();
+    else res.redirect('/login');
+};
+
+export const loggedOutOnly = (req, res, next) => {
+    if (req.isUnauthenticated()) next();
+    else res.redirect('/');
+};
+
+AuthRouter.post(
+    '/login',
+    (req, res, next) => {
+        const { username, password } = req.body;
+        if (!username) res.status(400).json({ msg: 'No username provided' });
+        if (!password) res.status(400).json({ msg: 'No password provided' });
+        next();
+    },
+    passport.authenticate(LOCAL_STRATEGY_KEY),
+    (req, res) => {
+        if (req.isAuthenticated) {
+            res.redirect('/');
+        } else {
+            res.status(401).end({ msg: 'Authentication failed' });
+        }
+    }
+);
+AuthRouter.post('/register', (req, res, next) => {
+    const { username, password } = req.body;
+    User.create({ username, password })
+        .then(user => {
+            req.login(user, err => {
+                if (err) next(err);
+                else res.redirect('/');
+            });
+        })
+        .catch(err => {
+            if (err.name === 'ValidationError') {
+                res.status(409).json('Username already taken');
+            } else next(err);
+        });
+});
+
+AuthRouter.post('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/login');
+});
+
+AuthRouter.get('/', (req, res) => {
+    if (req.isAuthenticated) {
+        res.json(req.user);
+    } else {
+        res.status(401).json({ msg: 'No user logged in' });
+    }
+});
+
+export default AuthRouter;
