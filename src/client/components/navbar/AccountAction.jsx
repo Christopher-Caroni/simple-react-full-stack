c396ba4b05ab7a9983c0546f46a0248383b51dd4
Christopher Caroni <christopher.caroni@etudiant.univ-lille1.fr>
2018-08-19 15:01:44 +0200

    added username and logout button to AccountAction

--- src/client/components/navbar/AccountAction.jsx
+++ src/client/components/navbar/AccountAction.jsx
@@ -2,12 +2,16 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import ReactRouterPropTypes from 'react-router-prop-types';
-import { Button, Menu } from 'semantic-ui-react';
+import { Button, Menu, Icon, Dropdown } from 'semantic-ui-react';
 
 import AdminkLinks from './AdminLinks';
 
 const AccountAction = ({ auth, history }) => {
-    const { authenticated, loginInProgress } = auth;
+    const {
+        authenticated,
+        loginInProgress,
+        user: { username },
+    } = auth;
 
     const link = (e, { href }) => {
         e.preventDefault();
@@ -19,13 +23,19 @@ const AccountAction = ({ auth, history }) => {
             return (
                 <>
                     <AdminkLinks />
+
                     <Menu.Item>
-                        <Button
-                            loading={loginInProgress}
-                            content="Login"
-                            href="/web/login"
-                            onClick={link}
-                        />
+                        <Icon name="user" />
+                        <Dropdown item text={username} pointing button>
+                            <Dropdown.Menu>
+                                <Dropdown.Item
+                                    icon="power off"
+                                    content="Logout"
+                                    href="/web/logout"
+                                    onClick={link}
+                                />
+                            </Dropdown.Menu>
+                        </Dropdown>
                     </Menu.Item>
                 </>
             );
@@ -56,6 +66,9 @@ AccountAction.propTypes = {
     auth: PropTypes.shape({
         loginInProgress: PropTypes.bool.isRequired,
         authenticated: PropTypes.bool.isRequired,
+        user: PropTypes.shape({
+            username: PropTypes.string.isRequired,
+        }),
     }).isRequired,
     history: ReactRouterPropTypes.history.isRequired,
 };
